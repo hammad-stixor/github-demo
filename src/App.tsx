40b70614c9b5eeaee5bf6bf19dc57d876bc2f3e1
hammad-stixor <hammad.umar@stixor.com>
2023-11-22 16:04:37 +0500

    add demo component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Demo from "./components/demo";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -32,6 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Demo />
     </>
   );
 }
---
+++ src/components/demo.tsx
@@ -0,0 +1,9 @@
+const Demo = () => {
+  return (
+    <div>
+      <h3>Demo </h3>
+    </div>
+  );
+};
+
+export default Demo;
